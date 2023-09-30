3c713c96838b0ff92b73e04250c9148346390c4e
Abdelatty <abdelattybadwy166@gmail.com>
2023-09-30 12:21:45 +0300

    update

--- Youtupe Clone/src/components/Header.jsx
+++ Youtupe Clone/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
-
+import { FiSearch } from "react-icons/fi";
 function Header() {
   const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState();
@@ -16,11 +16,12 @@ function Header() {
   return (
     <div className="bg-gray-900 text-white flex items-center justify-between">
       <div className="flex items-center justify-between">
-        <img className="w-16 m-4 ml-16" src="../../public/logo.png" />
+        <img className="w-16 m-4 ml-16" src="/logo.png" />
         <h1 className="text-red-500 text-xl font-bold italic hidden sm:block">Yout<span className="text-white">Tube</span></h1>
       </div>
-      <form onSubmit={onhandleSubmit}>
-        <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className="mr-16 rounded-xl w-[200px] p-1 text-gray-900 sm:w-[250px]" placeholder="search" type="text"></input>
+      <form onSubmit={onhandleSubmit} className="flex justify-between mr-16 items-center ">
+        <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className="mr-4 rounded-xl w-[200px] p-1 px-2 text-gray-900 sm:w-[250px]" placeholder="search" type="text"></input>
+        <FiSearch/>
       </form>
     </div>
   )
